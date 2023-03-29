195ca7a2e34320d976c883c748d5b1a5cae431a8
Fletcher Wilkens <fletcher.wilkens1@gmail.com>
2023-03-28 21:14:06 -0700

    Add GMI, AQI calcs

--- beneish.go
+++ beneish.go
@@ -4,41 +4,66 @@ import (
 	"github.com/shopspring/decimal"
 )
 
-// https://en.wikipedia.org/wiki/Beneish_M-score
 /*
+https://en.wikipedia.org/wiki/Beneish_M-score
 The Beneish M-score is calculated using 8 variables (financial ratios):[1][2]
-
-Days Sales in Receivables Index
-(DSRI) DSRI = (Net Receivables t / Sales t) / (Net Receivables t-1 / Sales t-1)
-
-Gross Margin Index (GMI)
-GMI = [(Sales t-1 - COGS t-1) / Sales t-1] / [(Sales t - COGSt) / Sales t]
-
-Asset Quality Index (AQI)
-AQI = [1 - (Current Assets t + PP&E t + Securities t) / Total Assets t] / [1 - ((Current Assets t-1 + PP&E t-1 + Securities t-1) / Total Assets t-1)]
-
-Sales Growth Index (SGI)
-SGI = Sales t / Sales t-1
-
-Depreciation Index (DEPI)
-DEPI = (Depreciation t-1/ (PP&E t-1 + Depreciation t-1)) / (Depreciationt / (PP&Et + Depreciationt))
-
-Sales General and Administrative Expenses Index (SGAI)
-SGAI = (SG&A Expense t / Sales t) / (SG&A Expense t-1 / Sales t-1)
-
-Leverage Index (LVGI)
-LVGI = [(Current Liabilities t + Total Long Term Debt t) / Total Assets t] / [(Current Liabilities t-1 + Total Long Term Debt t-1) / Total Assets t-1]
-
-Total Accruals to Total Assets (TATA)
-TATA = (Income from Continuing Operations t - Cash Flows from Operations t) / Total Assets t
 
 The formula to calculate the M-score is:[1]
 
 M-score = −4.84 + 0.92 × DSRI + 0.528 × GMI + 0.404 × AQI + 0.892 × SGI + 0.115 × DEPI −0.172 × SGAI + 4.679 × TATA − 0.327 × LVGI
 */
-func dsri(currNetReceivables int64, prevNetReceivables int64, currSales int64, prevSales int64) decimal.Decimal {
+
+// Days Sales in Receivables Index
+// (DSRI) DSRI = (Net Receivables t / Sales t) / (Net Receivables t-1 / Sales t-1)
+func dsri(prevNetReceivables int64, currNetReceivables int64, prevSales int64, currSales int64) decimal.Decimal {
 	numerator := decimal.New(currNetReceivables, 0).Div(decimal.New(currSales, 0))
 	denominator := decimal.New(prevNetReceivables, 0).Div(decimal.New(prevSales, 0))
 
 	return numerator.DivRound(denominator, 4)
 }
+
+// Gross Margin Index (GMI)
+// GMI = [(Sales t-1 - COGS t-1) / Sales t-1] / [(Sales t - COGSt) / Sales t]
+func gmi(prevSales int64, currSales int64, prevCogs int64, currCogs int64) decimal.Decimal {
+	numerator := decimal.New(prevSales-prevCogs, 0).Div(decimal.New(prevSales, 0))
+	denominator := decimal.New(currSales-currCogs, 0).Div(decimal.New(currSales, 0))
+
+	return numerator.DivRound(denominator, 4)
+}
+
+// Asset Quality Index (AQI)
+// AQI = [1 - (Current Assets t + PP&E t + Securities t) / Total Assets t] / = [1 - ((Current Assets t-1 + PP&E t-1 + Securities t-1) / Total Assets t-1)]
+func aqi(
+	prevAssets int64,
+	prevSecurities int64,
+	prevPPE int64,
+	prevTotalAssets int64,
+	currAssets int64,
+	currSecurities int64,
+	currPPE int64,
+	currTotalAssets int64,
+) decimal.Decimal {
+	currSum := currAssets + currSecurities + currPPE
+	currRatio := decimal.New(currSum, 0).Div(decimal.New(currTotalAssets, 0))
+	prevSum := prevAssets + prevPPE + prevSecurities
+	prevRatio := decimal.New(prevSum, 0).Div(decimal.New(prevTotalAssets, 0))
+	numerator := decimal.New(1, 0).Sub(currRatio)
+	denominator := decimal.New(1, 0).Sub(prevRatio)
+
+	return numerator.DivRound(denominator, 4)
+}
+
+// Sales Growth Index (SGI)
+// SGI = Sales t / Sales t-1
+
+// Depreciation Index (DEPI)
+// DEPI = (Depreciation t-1/ (PP&E t-1 + Depreciation t-1)) / (Depreciationt / (PP&Et + Depreciationt))
+
+// Sales General and Administrative Expenses Index (SGAI)
+// SGAI = (SG&A Expense t / Sales t) / (SG&A Expense t-1 / Sales t-1)
+
+// Leverage Index (LVGI)
+// LVGI = [(Current Liabilities t + Total Long Term Debt t) / Total Assets t] / [(Current Liabilities t-1 + Total Long Term Debt t-1) / Total Assets t-1]
+
+// Total Accruals to Total Assets (TATA)
+// TATA = (Income from Continuing Operations t - Cash Flows from Operations t) / Total Assets t
--- beneish_test.go
+++ beneish_test.go
@@ -14,7 +14,7 @@ func TestDsri(t *testing.T) {
 		t.Error(err)
 	}
 
-	result := dsri(100, 90, 200, 150)
+	result := dsri(90, 100, 150, 200)
 
 	if !result.Equals(expected) {
 		fmt.Println(result)
@@ -27,7 +27,51 @@ func TestDsri(t *testing.T) {
 		t.Error(err)
 	}
 
-	result = dsri(120, 50, 500, 400)
+	result = dsri(50, 120, 400, 500)
+
+	if !result.Equals(expected) {
+		fmt.Println(result)
+		t.Errorf("expected %q, got %q", expected, result)
+	}
+}
+
+// GMI = [(Sales t-1 - COGS t-1) / Sales t-1] / [(Sales t - COGSt) / Sales t]
+func TestGmi(t *testing.T) {
+	expected, err := decimal.NewFromString("0.6667")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	result := gmi(90, 100, 60, 50)
+
+	if !result.Equals(expected) {
+		fmt.Println(result)
+		t.Errorf("expected %q, got %q", expected, result)
+	}
+
+	expected, err = decimal.NewFromString("1.5000")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	result = gmi(100, 90, 50, 60)
+
+	if !result.Equals(expected) {
+		fmt.Println(result)
+		t.Errorf("expected %q, got %q", expected, result)
+	}
+}
+
+func TestAqi(t *testing.T) {
+	expected, err := decimal.NewFromString("1.0714")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	result := aqi(100, 200, 60, 600, 130, 210, 60, 700)
 
 	if !result.Equals(expected) {
 		fmt.Println(result)
